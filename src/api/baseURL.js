6afee21172bd9a0ec171a1ed174212ef7873b832
tiwariaditya15 <40066667+tiwariaditya15@users.noreply.github.com>
2022-06-15 09:30:05 +0530

    fix: base url

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,2 +1,4 @@
 export const URL = "https://tennistown.herokuapp.com";
 // "http://localhost:5000";
+// "https://tennistown-be.tiwariaditya.repl.co";
+// "https://tennistown-be-1.tiwariaditya.repl.co";
