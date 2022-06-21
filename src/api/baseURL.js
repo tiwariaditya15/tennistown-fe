40a7474a71ad6da490acc0c13aa17b669e0d8d5b
Aditya Tiwari <tiwariaditya15@hotmail.com>
2022-06-21 23:18:23 +0530

    refactor: changed URL & hide url

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,4 +1,1 @@
-export const URL = "https://tennistown-be-1.tiwariaditya.repl.co";
-// "http://localhost:5000";
-// "https://tennistown.herokuapp.com";
-// "https://tennistown-be.tiwariaditya.repl.co";
+export const URL = process["env"]["REACT_APP_ENDPOINT"];
